ae120c48c36f8f34b74b14664ac89a5284e73039
Anh Tuan <atn832@gmail.com>
2022-12-11 20:10:25 +0900

    better document PenRenderer.draw

--- lib/src/pen_renderer/pen_renderer.dart
+++ lib/src/pen_renderer/pen_renderer.dart
@@ -1,14 +1,9 @@
 import 'dart:ui';
 
-/// A Function that renders a Pen.
+/// A class that renders a Pen.
 abstract class PenRenderer {
-  ///
-  void draw(
-      Canvas canvas,
-
-      /// Position of the pen.
-      Offset penPosition,
-
-      /// The Path's current [Paint].
-      Paint currentPaint);
+  /// Draws a Pen.
+  /// @param penPosition Position of the pen.
+  /// @param currentPaint The Path's current [Paint].
+  void draw(Canvas canvas, Offset penPosition, Paint currentPaint);
 }
