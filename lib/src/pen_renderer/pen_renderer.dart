e85c48a76902979e70f561b52a143c4198552067
Anh Tuan <atn832@gmail.com>
2022-12-12 10:40:15 +0900

    fixed PenRenderer docs

--- lib/src/pen_renderer/pen_renderer.dart
+++ lib/src/pen_renderer/pen_renderer.dart
@@ -2,8 +2,7 @@ import 'dart:ui';
 
 /// A class that renders a Pen.
 abstract class PenRenderer {
-  /// Draws a Pen.
-  /// @param penPosition Position of the pen.
-  /// @param currentPaint The Path's current [Paint].
+  /// Draws a Pen. The drawing animation ends at [penPosition] using
+  /// [currentPaint]. It's up to the [PenRenderer]s to use them.
   void draw(Canvas canvas, Offset penPosition, Paint currentPaint);
 }
